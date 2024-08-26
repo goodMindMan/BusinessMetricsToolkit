09b70cd9ed6df304753f73826ed46a90e1024a76
Sawwab <125457215+goodMindMan@users.noreply.github.com>
2024-08-27 00:23:32 +0300

    Update revenue.py

--- accountingCycleToolkit/partnership/account_types/revenue.py
+++ accountingCycleToolkit/partnership/account_types/revenue.py
@@ -5,52 +5,50 @@ from ..root_account import Account
 class Revenue(Account):
     def __init__(self, pr:int, name:str):
         super().__init__(pr, name)
-    
-    def account_type(self):
-        return 'revenue'
-        
-    def balance(self):
-        dr_sum = self.ledger['Debit'].sum()
-        cr_sum = self.ledger['Credit'].sum()
-        balance = cr_sum - dr_sum 
-        return balance
-    
+        self.balance = self.ledger['Credit'].sum() - self.ledger['Debit'].sum()
+        self.account_type = 'revenue'
+
     def show_ledger(self):
-        balance_row = pd.DataFrame([['Balance:', '-', self.balance()]], columns=self.columns)
-        return pd.concat([self.ledger, balance_row], ignore_index=True)
+        '''
+        Credit is the normal balance of `Revenue` accounts, that is y the left field is empty i.e. `'-'`
+        Space and Time complexity of O(mn) in this case 3*3
+        '''
+        balance_row = pd.DataFrame([['Balance:', '-', self.balance()]], columns=self.columns) #O(mn)
+        return pd.concat([self.ledger, balance_row], ignore_index=True) #O(n)
 
 class sales(Revenue):  
-    def account_type(self):
-        return 'sales'
+    def __init__(self, pr:int, name:str, balance:float, account_type:str):
+        super().__init__(pr, name, balance ,account_type)
+        self.account_type = 'sales'
 
 class GainDisposal(Revenue):  
-    def account_type(self):
-        return 'gain_disposal'
+    def __init__(self, pr:int, name:str, balance:float, account_type:str):
+        super().__init__(pr, name, balance ,account_type)
+        self.account_type = 'gain_disposal'
     
 class ContraRev(Account):
     def __init__(self, pr:int, name:str):
         super().__init__(pr, name)
+        self.balance = ['Debit'].sum() - ['Credit'].sum()
+        self.account_type = 'contra_revenue'
     
-    def account_type(self):
-        return 'contra_revenue'
-    
-    def balance(self):
-        dr_sum = self.ledger['Debit'].sum()
-        cr_sum = self.ledger['Credit'].sum()
-        balance = dr_sum - cr_sum
-        return balance
-    
-    def show_ledger(self):
-        balance_row = pd.DataFrame([['Balance:', self.balance(), '-']], columns=self.columns)
-        return pd.concat([self.ledger, balance_row], ignore_index=True)
+    def show_ledger(self): #O(mn)
+        '''
+        Debit is the normal balance of ContraRev accounts, that is y the right feild is empty i.e. `'-'`
+        Space and Time complexity of O(mn) in this case 3*3
+        '''
+        balance_row = pd.DataFrame([['Balance:', self.balance(), '-']], columns=self.columns) #O(mn)
+        return pd.concat([self.ledger, balance_row], ignore_index=True) #O(n)
 
 class Discount(ContraRev):      
-    def account_type(self):
-        return 'discount'
+    def __init__(self, pr:int, name:str, balance:float, account_type:str):
+        super().__init__(pr, name, balance ,account_type)
+        self.account_type = 'discount'
 
 class ReturnsAllowances(ContraRev):
-    def account_type(self):
-        return 'returns_allowances'
+    def __init__(self, pr:int, name:str, balance:float, account_type:str):
+        super().__init__(pr, name, balance ,account_type)
+        self.account_type = 'returns_allowances'
 
 revenue_accounts = [
         'sales', 
