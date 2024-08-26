3982cab8c1a8840ca681feacf982a28b6532736a
Sawwab <125457215+goodMindMan@users.noreply.github.com>
2024-08-27 00:22:22 +0300

    Update capital.py
    - Made the class attributes instance attributes
    - Made some methods that only returned a string instance attributes
    - Added some comments cus y NOT.

--- accountingCycleToolkit/partnership/account_types/capital.py
+++ accountingCycleToolkit/partnership/account_types/capital.py
@@ -1,37 +1,31 @@
 import pandas as pd
 
-from ..root_account import Account
+from ..root_account import Account #Summons the abstract account
 
 class Equity(Account):
     def __init__(self, pr:int, name:str):
         super().__init__(pr, name)
-    
-    def account_type(self):
-        return 'equity'
-    
-    def balance(self):
-        dr_sum = self.ledger['Debit'].sum()
-        cr_sum = self.ledger['Credit'].sum()
-        balance = cr_sum - dr_sum 
-        return balance
+        self.balance = self.ledger['Credit'].sum() - self.ledger['Debit'].sum()
+        self.account_type = 'equity'
     
     def show_ledger(self):
+        '''
+        Credit is the normal balance of Equity accounts, that is y the left feild is empty i.e. `'-'`
+        Space and Time complexity of O(mn) in this case 3*3
+        '''       
         balance_row = pd.DataFrame([['Balance:', '-', self.balance()]], columns=self.columns)
         return pd.concat([self.ledger, balance_row], ignore_index=True)
 
 class Withdrawals(Equity):
     def __init__(self, pr:int, name:str):
         super().__init__(pr, name)
-    
-    def account_type(self):
-        return 'withdrawal'
-      
-    def balance(self):
-        dr_sum = self.ledger['Debit'].sum()
-        cr_sum = self.ledger['Credit'].sum()
-        balance = dr_sum - cr_sum
-        return balance
-    
+        self.balance = self.ledger['Debit'].sum() - self.ledger['Credit'].sum()
+        self.account_type = 'withdrawal'
+
     def show_ledger(self):
+        '''
+        Debit is the normal balance of Withdrawal accounts, that is y the right feild is empty i.e. `'-'`
+        Space and Time complexity of O(mn) in this case 3*3
+        '''
         balance_row = pd.DataFrame([['Balance:', self.balance(), '-']], columns=self.columns)
         return pd.concat([self.ledger, balance_row], ignore_index=True)
